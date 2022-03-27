4a2911ce9cf99960a074d840d1b3975e59b33cf7
CHRISCUI\Cinne <chriscui47@yahoo.com>
2022-03-27 15:17:37 -0400

    make

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/api', api);
 
 /** Rules of our API */
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
